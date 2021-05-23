65e25c172bdc1ab134452bfb469b97b08cb83350
Joe Cabezas Campos <joe.cabezas@gmail.com>
2021-05-22 21:37:45 -0700

    removed adalotl model class

--- stakepoolcentral/stakepoolcentral/spiders/tx_metadata_searchresults.py
+++ stakepoolcentral/stakepoolcentral/spiders/tx_metadata_searchresults.py
@@ -2,17 +2,21 @@ import json
 import lxml.html
 import scrapy
 
-from adalotl import Adalotl
-
 class TxMetadataSearchresultsSpider(scrapy.Spider):
     name = 'tx_metadata_searchresults'
     allowed_domains = ['stakepoolcentral.com']
     start_urls = ['https://bi.stakepoolcentral.com/tx_metadata_searchresults?query=Adalotl%20293']
 
+    def start_requests(self):
+        yield scrapy.Request(f"https://bi.stakepoolcentral.com/tx_metadata_searchresults?query=Adalotl%20{self.adalotl_number}")
+
     def parse(self, response):
         html = response.xpath('//pre').get()
+
+        if not html:
+            raise RuntimeError(f"metadata for {self.adalotl_number} not found")
+
         tree = lxml.html.fromstring(html)
         json_string = tree.text_content()
-        metadata = json.loads(json_string)
-        adalotl = Adalotl(metadata)
-        print(adalotl)
+        return json.loads(json_string)
+
